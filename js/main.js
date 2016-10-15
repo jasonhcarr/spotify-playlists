d20869ba56105de8dc82fd27619feee9ab9336a5
Jason Carr <jasonhcarr@gmail.com>
2016-10-15 18:07:15 -0400

    saturday evening

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 //VARIABLE DECLARATIONS
-
+var testResponsePlaylistDetails = {
+    collaborative: false,
+    description: null,
+    external_urls: {
+        spotify: "http://open.spotify.com/user/1221442507/playlist/3Eruo4sHfpSx7yIVU6HCih"
+    },
+    followers: {
+        ref: null,
+        total: 0
+    },
+    href: "https://api.spotify.com/v1/users/1221442507/playlists/3Eruo4sHfpSx7yIVU6HCih",
+    id: "3Eruo4sHfpSx7yIVU6HCih",
+    images:  [{
+          height: 640,
+            url: "https://mosaic.scdn.co/640/9841999eddaf438d6394a1187d3a144ff8780a6bd42d8d08a15662be1d653931d1f123df5698a344ff14fe8680be9a3378abac7026cc29bdf028717876c6922d5807df334116c10c512a4eed6dc0eab9",
+            width: 640,
+            __proto__: Object
+          },
+            {
+            height: 300,
+            url: "https://mosaic.scdn.co/300/9841999eddaf438d6394a1187d3a144ff8780a6bd42d8d08a15662be1d653931d1f123df5698a344ff14fe8680be9a3378abac7026cc29bdf028717876c6922d5807df334116c10c512a4eed6dc0eab9",
+            width: 300,
+            __proto__: Object
+          },
+          {
+            height: 60,
+            url: "https://mosaic.scdn.co/60/9841999eddaf438d6394a1187d3a144ff8780a6bd42d8d08a15662be1d653931d1f123df5698a344ff14fe8680be9a3378abac7026cc29bdf028717876c6922d5807df334116c10c512a4eed6dc0eab9",
+            width: 60,
+            __proto__: Object
+          },
+        {length: 3
+        }
+    ],
+    name: "Punkish",
+    owner: Object,
+    public: true,
+    snapshot_id: "dTlCPwB5TpVd4/JD28CFalFaCre1EAQ4bM/kKdHMD1BLg3hzxlPPJqlk0YXNgIpO",
+    tracks: {
+        href: "https://api.spotify.com/v1/users/1221442507/playlists/3Eruo4sHfpSx7yIVU6HCih/tracks?offset=0&limit=100&market=ES"
+    }
+};
 //CONSTRUCTORS
 
 //FUNCTIONS

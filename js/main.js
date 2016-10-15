114c0c226ca5ec290ae5f493d8a9d50410befd8f
Jason Carr <jasonhcarr@gmail.com>
2016-10-15 18:12:16 -0400

    saturday again

--- js/main.js
+++ js/main.js
@@ -1,49 +1,113 @@
 //VARIABLE DECLARATIONS
-var testResponsePlaylistDetails = {
-    collaborative: false,
-    description: null,
-    external_urls: {
-        spotify: "http://open.spotify.com/user/1221442507/playlist/3Eruo4sHfpSx7yIVU6HCih"
-    },
-    followers: {
-        ref: null,
-        total: 0
-    },
-    href: "https://api.spotify.com/v1/users/1221442507/playlists/3Eruo4sHfpSx7yIVU6HCih",
-    id: "3Eruo4sHfpSx7yIVU6HCih",
-    images:  [{
-          height: 640,
-            url: "https://mosaic.scdn.co/640/9841999eddaf438d6394a1187d3a144ff8780a6bd42d8d08a15662be1d653931d1f123df5698a344ff14fe8680be9a3378abac7026cc29bdf028717876c6922d5807df334116c10c512a4eed6dc0eab9",
-            width: 640,
-            __proto__: Object
-          },
-            {
-            height: 300,
-            url: "https://mosaic.scdn.co/300/9841999eddaf438d6394a1187d3a144ff8780a6bd42d8d08a15662be1d653931d1f123df5698a344ff14fe8680be9a3378abac7026cc29bdf028717876c6922d5807df334116c10c512a4eed6dc0eab9",
-            width: 300,
-            __proto__: Object
-          },
-          {
-            height: 60,
-            url: "https://mosaic.scdn.co/60/9841999eddaf438d6394a1187d3a144ff8780a6bd42d8d08a15662be1d653931d1f123df5698a344ff14fe8680be9a3378abac7026cc29bdf028717876c6922d5807df334116c10c512a4eed6dc0eab9",
-            width: 60,
-            __proto__: Object
-          },
-        {length: 3
-        }
-    ],
-    name: "Punkish",
-    owner: Object,
-    public: true,
-    snapshot_id: "dTlCPwB5TpVd4/JD28CFalFaCre1EAQ4bM/kKdHMD1BLg3hzxlPPJqlk0YXNgIpO",
-    tracks: {
-        href: "https://api.spotify.com/v1/users/1221442507/playlists/3Eruo4sHfpSx7yIVU6HCih/tracks?offset=0&limit=100&market=ES"
-    }
-};
+
 //CONSTRUCTORS
 
 //FUNCTIONS
 
 //CALLS
+(function() {
+
+  var stateKey = 'spotify_auth_state';
+
+  /**
+   * Obtains parameters from the hash of the URL
+   * @return Object
+   */
+  function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+  /**
+   * Generates a random string containing numbers and letters
+   * @param  {number} length The length of the string
+   * @return {string} The generated string
+   */
+  function generateRandomString(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+  var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+      userProfileTemplate = Handlebars.compile(userProfileSource),
+      userProfilePlaceholder = document.getElementById('user-profile');
+
+      oauthSource = document.getElementById('oauth-template').innerHTML,
+      oauthTemplate = Handlebars.compile(oauthSource),
+      oauthPlaceholder = document.getElementById('oauth');
+
+  var params = getHashParams();
+
+  var access_token = params.access_token,
+      state = params.state,
+      storedState = localStorage.getItem(stateKey);
+
+  if (access_token && (state == null || state !== storedState)) {
+    alert('There was an error during the authentication');
+  } else {
+    localStorage.removeItem(stateKey);
+    if (access_token) {
+      $.ajax({
+          url: 'https://api.spotify.com/v1/me',
+          headers: {
+            'Authorization': 'Bearer ' + access_token
+          },
+          success: function(response) {
+            userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+
+            $('#login').hide();
+            $('#loggedin').show();
+          }
+      });
+    } else {
+        $('#login').show();
+        $('#loggedin').hide();
+    }
+
+    document.getElementById('login-button').addEventListener('click', function() {
+
+      var client_id = '4e84e49e334f411e95add4fad7edf9e7'; // Your client id
+      var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+      var state = generateRandomString(16);
+
+      localStorage.setItem(stateKey, state);
+      var scope = 'user-read-private user-read-email';
+
+      var url = 'https://accounts.spotify.com/authorize';
+      url += '?response_type=token';
+      url += '&client_id=' + encodeURIComponent(client_id);
+      url += '&scope=' + encodeURIComponent(scope);
+      url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+      url += '&state=' + encodeURIComponent(state);
+
+      window.location = url;
+    }, false);
+  }
+})();
+
+$.ajax({
+    url: 'https://api.spotify.com/v1/users/1221442507/playlists/3Eruo4sHfpSx7yIVU6HCih?market=ES',
+    headers: {
+      'Authorization': 'Bearer ' + access_token
+    },
+    success: function(response) {
+      console.log(response);
+      userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+
+      $('#login').hide();
+      $('#loggedin').show();
+    }
+});
 
 //EVENT DELEGATORS

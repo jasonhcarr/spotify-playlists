0e2683d7decb7f54113f9f824c8a0b73f86f8424
Jason Carr <jasonhcarr@gmail.com>
2016-10-14 21:10:34 -0400

    first api in place

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -12,9 +12,9 @@ var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '4e84e49e334f411e95add4fad7edf9e7'; // Your client id
+var client_secret = '379cfedbe166463b820078c519330c3e'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
